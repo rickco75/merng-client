13af4c19a1858a184684ac3319139d55e73bf501
Rick Cohen <rickco@gmail.com>
2020-10-25 14:25:02 -0500

    edit prox and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://mighty-caverns-32856.herokuapp.com/'
 })
 
 const authLink = setContext(()=>{
